590e7e790c8053c6cd33a71d0916c9572a8c18a0
Rafal Maruszewski <rmaruszews001@cee.ema.ad.pwcinternal.com>
2019-03-28 14:07:12 +0100

    created function that returns random word from database

--- Hangman.py
+++ Hangman.py
@@ -7,16 +7,13 @@ c = conn.cursor()
 #TODO: set range depends on database size
 random_index = random.randrange(1,10)
 
-print("Selected random number: ", random_index)
-random_word = c.execute("SELECT word FROM words WHERE no=1", (random_index,))
-print("Random word from database: ", c.fetchone())
 
-def get_word(word):
-    print("HI!")
-    
-#def get_random_word():
-    
-#    print (random_word)
 
-#print(c.execute("SELECT * FROM words").fetchall())
+def get_random_word():
+    print("Selected random number: ", random_index)
+    random_word = c.execute("SELECT word FROM words WHERE no=(?)", (random_index,))
+    print("Random word from database: ", c.fetchone())
+    return random_word
+    
+get_random_word()
    
