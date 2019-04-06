3c8e03ed73f60cd0c5ab9720a5b6bbec364734d2
Rafal Maruszewski <rmaruszews001@cee.ema.ad.pwcinternal.com>
2019-04-06 21:44:06 +0200

    Fixed lists, guessing mechanism almost working, only no bad guesses counter

--- Hangman.py
+++ Hangman.py
@@ -4,7 +4,7 @@ import random
 
 conn = sqlite3.connect('hangman_db')
 c = conn.cursor()
-#TODO: set range depends on database size
+#TODO: set range depending on database size
 random_index = random.randrange(1,10)
 
 
@@ -16,20 +16,29 @@ def get_random_word():
     print("Random word from database: ", random_word)
     return list(random_word)
     
-def make_a_guess(random_word):
-    guess = input()
-    guess = guess.lower()
-    #for item in random_word:
-    #    for letter in item:
-    #        if guess == letter:
-    #            print("+1") 
-    #        else:
-    #            print("Błąd!")
+def transform_word_to_str(random_word):
+    random_w_str = ''.join(random_w)
+    return random_w_str
 
 random_w = get_random_word()
 print("word:", random_w)
-random_w_str = ''.join(random_w)
+random_w_str = transform_word_to_str(random_w)
 print("After transform: ", random_w_str)
+empty_random_w_str = []
 for i in range(len(random_w_str)):
-    print("_", end = " ")
-make_a_guess(random_w)
+    empty_random_w_str.append("_")
+    
+print(empty_random_w_str)
+
+it1 = 0
+it2 = 0
+
+guess = input()
+for item in random_w_str:
+    
+    if item == guess:
+        print ("Zgadłeś literę ", guess)
+        empty_random_w_str[it1] = guess
+
+    it1+=1
+print(empty_random_w_str)
